4b33ae684ce24090c688f1d4cefd293c8a348bee
Kandor685 <k22white@student.ccn.ac.uk>
2025-08-26 11:41:57 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,15 +1,1 @@
-print ("Hello! Welcome to the program!")
-def details():
-    name = input("What is your name? ")
-    print ("Hello,", name, "!")
-    age = input("How old are you? ")
-    Location = input ("Where do you live? (Roughly) ")
-    confirm = input ("So your name is", name, "and you are", age, "years old and you live in", Location, "is that correct?")
-    if confirm == "yes" or confirm == "Yes":
-        print ("Good to know!")
-        quit()
-    else:
-        print ("I'm sorry! Let's start again!")
-        details()
-details()
-quit()
+print("Hello World!")
